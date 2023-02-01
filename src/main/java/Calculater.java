4e7a54c40b006748e4c1ae7e7f19fa9352030fd6
MrSalim86 <cph-ms1068@cphbusiness.dk>
2023-02-01 13:00:27 +0100

    first commit

---
+++ src/main/java/Calculater.java
@@ -0,0 +1,13 @@
+public class Calculater {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculater calculater = new Calculater();
+        System.out.println(calculater.add(1,2));
+
+    }
+
+
+}
---
+++ src/test/java/CalculaterTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculaterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    public void testAdd(){
+        System.out.println("testAdd");
+        Calculater calculater = new Calculater();
+        int actuel = calculater.add(1,2);
+        int expected = 3;
+        assertEquals(expected,actuel);
+
+    }
+}
